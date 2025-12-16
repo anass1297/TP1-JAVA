5fd72e6683ce76500a0d2af5b793aa4ed4e6d632
anass1297 <anasskacm441@gmail.com>
2025-12-16 19:17:32 +0100

    first commit

---
+++ src/EXERCICE1/Main.java
@@ -0,0 +1,17 @@
+package EXERCICE1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("=== MENU PRINCIPAL ===");
+		System.out.println("1. Addition");
+		System.out.println("2. Multiplication");
+		System.out.println("3. Moyenne");
+		System.out.println("4. Trouver le plus grand");
+		System.out.println("0. Quitter");
+	}
+		
+	}
+
+	
