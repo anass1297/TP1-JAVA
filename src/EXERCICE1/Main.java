6c87d57bbe6ea1669fa8ea8ba31e17d366a57aa6
anass1297 <anasskacm441@gmail.com>
2025-12-16 19:47:34 +0100

    first commit

--- src/EXERCICE1/Main.java
+++ src/EXERCICE1/Main.java
@@ -1,5 +1,7 @@
 package EXERCICE1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,8 +12,101 @@ public class Main {
 		System.out.println("3. Moyenne");
 		System.out.println("4. Trouver le plus grand");
 		System.out.println("0. Quitter");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int choix;
+		do {
+		    System.out.print("Choisissez une option : ");
+		    choix = sc.nextInt();
+		    
+		    switch (choix) {
+		    case 1:
+		        System.out.print("Entrez deux entiers : ");
+		        int a1 = sc.nextInt();
+		        int b1 = sc.nextInt();
+		        System.out.println("Résultat : " + addition(a1, b1));
+		        break;
+
+		    case 2:
+		        System.out.print("Entrez deux entiers : ");
+		        int a2 = sc.nextInt();
+		        int b2 = sc.nextInt();
+		        System.out.println("Résultat : " + multiplication(a2, b2));
+		        break;
+
+		    case 3:
+		        System.out.print("Combien de valeurs ? ");
+		        int n = sc.nextInt();
+		        int[] valeurs = new int[n];
+		        for (int i = 0; i < n; i++) {
+		            System.out.print("Valeur " + (i + 1) + " : ");
+		            valeurs[i] = sc.nextInt();
+		        }
+		        System.out.println("Moyenne : " + moyenne(valeurs));
+		        break;
+
+		    case 4:
+		        System.out.print("Combien de nombres ? ");
+		        int m = sc.nextInt();
+		        int[] entiers = new int[m];
+		        for (int i = 0; i < m; i++) {
+		            System.out.print("Nombre " + (i + 1) + " : ");
+		            entiers[i] = sc.nextInt();
+		        }
+		        System.out.println("Maximum : " + maximum(entiers));
+		        break;
+
+		    case 0:
+		        System.out.println("Fin du programme.");
+		        break;
+
+		    default:
+		        System.out.println("Option invalide !");
+		}
+		    
+		    
+		} while (choix != 0);
+		
+		
+		//------------------------------------------------
+		
+		
+		
+		
+		//-----------------------------------------
+	
+		
 	}
 		
+	public static int addition(int a, int b) {
+	    return a + b;
+	}
+	
+	public static int multiplication(int a, int b) {
+	    return a * b;
+	}
+	
+	public static double moyenne(int... valeurs) {
+	    int total = 0;
+	    for (int v : valeurs) {
+	        total += v;
+	    }
+	    return (double) total / valeurs.length;
+	}
+	
+	public static int maximum(int... valeurs) {
+	    int max = valeurs[0];
+	    for (int v : valeurs) {
+	        if (v > max) {
+	            max = v;
+	        }
+	    }
+	    return max;
+	}
+	
+	
+	
 	}
 
 	
